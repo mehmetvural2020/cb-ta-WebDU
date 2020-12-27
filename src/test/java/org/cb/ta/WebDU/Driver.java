aabdfb5320bb648e638c6cbe60ca3830973e8e77
mehmetvural2020 <mehmetvurall@gmail.com>
2020-12-28 05:00:35 +0700

    Driver.java file code commented.

--- src/test/java/org/cb/ta/WebDU/Driver.java
+++ src/test/java/org/cb/ta/WebDU/Driver.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.WebDriver;
 
 public class Driver {
 
-    public static WebDriver getDriver() {
-        System.setProperty("webdriver.chrome.driver", "../webdriver/chromedriver");
-        return null;
+//    public static WebDriver getDriver() {
+//        System.setProperty("webdriver.chrome.driver", "../webdriver/chromedriver");
+//        return null;
     }
 }
