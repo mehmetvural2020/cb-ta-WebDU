2a9217f58363657abd6122fa516eb2e7ca23818d
mehmetvural2020 <mehmetvurall@gmail.com>
2021-01-04 02:28:20 +0700

    DropDownCheckBoxRadioBtnsPage.java file created.

--- src/test/java/org/cb/ta/WebDU/Driver.java
+++ src/test/java/org/cb/ta/WebDU/Driver.java
@@ -1,11 +1,14 @@
 package org.cb.ta.WebDU;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
 
-//    public static WebDriver getDriver() {
-//        System.setProperty("webdriver.chrome.driver", "../webdriver/chromedriver");
-//        return null;
+    public static WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver", "../webdriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
     }
 }
