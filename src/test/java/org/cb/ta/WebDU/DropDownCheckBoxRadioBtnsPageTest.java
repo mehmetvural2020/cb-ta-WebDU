7da2ec67f23b3db7ca4855fab8c997b74cf5060d
mehmetvural2020 <mehmetvurall@gmail.com>
2021-01-04 03:49:35 +0700

    DropDownCheckBoxRadioBtnsPageTest.java file created.

---
+++ src/test/java/org/cb/ta/WebDU/DropDownCheckBoxRadioBtnsPageTest.java
@@ -0,0 +1,69 @@
+package org.cb.ta.WebDU;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+public class DropDownCheckBoxRadioBtnsPageTest extends BaseTest{
+
+    DropDownCheckBoxRadioBtnsPage dropDownCheckBoxRadioBtnsPage;
+
+    public DropDownCheckBoxRadioBtnsPageTest() {
+        this.dropDownCheckBoxRadioBtnsPage = new DropDownCheckBoxRadioBtnsPage(driver);
+    }
+
+    @Test
+    public void testSelectAnOptionFromDropDownMenu() {
+        WebElement selectedOption = dropDownCheckBoxRadioBtnsPage.selectAnOptionFromDropDownMenu("python");
+        assertEquals(selectedOption.getAttribute("value"), "python");
+    }
+
+    @Test
+    public void testSelectACheckBox() {
+        List<WebElement> checkBoxes = dropDownCheckBoxRadioBtnsPage.selectACheckBox();
+
+        for (WebElement element : checkBoxes) {
+            if (element.isSelected()) continue;
+
+            element.click();
+            System.out.println(element.getText() + " is clicked!");
+            assertTrue(element.isSelected());
+        }
+    }
+
+    @Test(dependsOnMethods = "testSelectACheckBox")
+    public void testdeSelectACheckBox() {
+        List<WebElement> checkBoxes = dropDownCheckBoxRadioBtnsPage.selectACheckBox();
+
+        for (WebElement element : checkBoxes) {
+            //if (element.isSelected()) continue;
+
+            element.click();
+            System.out.println(element.getText() + " is deselected!");
+            assertFalse(element.isSelected());
+        }
+
+    }
+
+    @Test
+    public void testGetRadioBtns() {
+        List<WebElement> radioBtns = dropDownCheckBoxRadioBtnsPage.getRadioBtns();
+
+        for (int i = 0; i < radioBtns.size(); i++) {
+            List<WebElement> tmprList = radioBtns;
+            radioBtns.get(i).click();
+            assertTrue(radioBtns.get(i).isSelected());
+            tmprList.remove(i);
+            int j = i + 1;
+            while(j < tmprList.size()) {
+                assertFalse(tmprList.get(j).isSelected());
+                j++;
+            }
+        }
+    }
+}
---
+++ src/test/java/org/cb/ta/WebDU/WDUDataTablesButtonStatesPageTest.java
@@ -0,0 +1,66 @@
+package org.cb.ta.WebDU;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+public class WDUDataTablesButtonStatesPageTest extends BaseTest {
+
+    private WDUDataTablesButtonStatesPage wduDataTablesButtonStatesPage;
+    private WriteDataToExcel writeDataToExcel = new WriteDataToExcel();
+
+    public WDUDataTablesButtonStatesPageTest() {
+        wduDataTablesButtonStatesPage = new WDUDataTablesButtonStatesPage(driver);
+    }
+
+    @Test
+    public void simple() {
+        String textOfHeader =  wduDataTablesButtonStatesPage.getHeaderElement().getText();
+//        System.out.println(textOfHeader);
+        assertEquals("Data, Tables & Button States", textOfHeader);
+    }
+
+    @Test
+    public void testTable1() throws IOException {
+        List<WebElement> rows = wduDataTablesButtonStatesPage.getTable1Rows();
+//        String[] dataToWrite = new String[4];
+        List<String[]> dataList = new ArrayList<>();
+        for (WebElement row : rows) {
+            String[] splittedRow = row.getText().split(" ");
+//            ["Firstname", "Lastname", "Age"]
+            dataList.add(splittedRow);
+//            System.out.println(row.getText());
+        }
+
+        // [["Firstname", "Lastname", "Age"],["John", "Smith", "45"],["Jemma", "Jackson", "94"],["Michael", "Doe", "20"]]
+        writeDataToExcel.writeExcel("src/test/java/org/cb/ta/resources/", "test01.xls", "Sayfa1", dataList );
+
+        /*
+         * Firstname lastname Age
+         * sally     abc       20
+         * john      xyz       30
+         *
+         * websitesinden cekilen veriler ile text boxlara yazilip table olmasini bekledigimiz verileri karsilastiralim
+         * bunun icin
+         * 1- textboxlara input gir
+         * 2- submitlemeye calis
+         * 3- tekrar veri cek
+         * 4- son satirlari karsilastir ve
+         * 5- mesaj don.
+         *
+         *
+         * */
+
+        // writeDataToExcel.writeExcel("src/test/java/org/cb/ta/resources/", "test01.xls", "Table01",  );
+
+
+    }
+}
