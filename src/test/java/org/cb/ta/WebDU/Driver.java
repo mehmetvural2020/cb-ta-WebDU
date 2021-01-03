2e04ef49e0ef67d58f374021b294b7a142653354
mehmetvural2020 <mehmetvurall@gmail.com>
2021-01-04 03:26:35 +0700

    Driver.java file updated.

--- src/test/java/org/cb/ta/WebDU/Driver.java
+++ src/test/java/org/cb/ta/WebDU/Driver.java
@@ -7,8 +7,8 @@ public class Driver {
 
     public static WebDriver getDriver() {
         System.setProperty("webdriver.chrome.driver", "../webdriver/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        return driver;
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+        return new ChromeDriver();
     }
 }
